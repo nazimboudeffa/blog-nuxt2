aa915b93cbe6335dde0f0a443627929515584230
nazimboudeffa <boudeffa@gmail.com>
2022-08-30 13:56:02 +0200

    Added blog page

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <header>
 	  <div class="title">My Blog</div>
 	  <nav>
-		  <nuxt-link to="/">Home</nuxt-link>
+		  <nuxt-link to="/blog">Blog</nuxt-link>
 		  <nuxt-link to="/about">About</nuxt-link>
 	  </nav>
   </header>
