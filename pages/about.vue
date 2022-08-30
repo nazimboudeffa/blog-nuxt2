c66f5c51c160059c1b383292fafdd13032555f4e
nazimboudeffa <boudeffa@gmail.com>
2022-08-30 14:36:07 +0200

    Added CSS to About PAge

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about-page">
-	  <h1>About Page</h1>
+	  <h1 class="about">About Page</h1>
   </div>
 </template>
 
@@ -11,5 +11,19 @@ export default {
 </script>
 
 <style>
+.about-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.about {
+  margin: 50px 30px;
+  width: 500px;
+  height: 500px;
+  background-color: gray;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
