6fc09b81da03b79fb5981b061a5bcc8256fa8413
Nazim Boudeffa <boudeffa@gmail.com>
2022-09-20 17:28:15 +0200

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="about-page">
 	  <h1 class="about">About Page</h1>
+    <h2>Learn</h2>
     <ul>
+    <li>https://www.codecademy.com/profiles/nazimboudeffa</li>
+    <li>https://www.freecodecamp.org/nazimboudeffa</li>
+    </ul>
+    <h2>Practice</h2>
+    <ul>
+      <ul>
       <li>https://cssbattle.dev/player/gXf2AZmAnoPeE0IYFOiv0MLn2sx2</li>
       <li>https://www.codewars.com/users/boudeffa</li>
       <li>https://binarysearch.com/@/nazimboudeffa</li>
+      <li>https://tryhackme.com/p/thefailtheory</li>
+      <li>https://www.root-me.org/thefailtheory</li>
+      </ul>
     </ul>
+    <h2>More social</h2>
+    <p>https://nazimboudeffa-social.glitch.me</p>
   </div>
 </template>
 
