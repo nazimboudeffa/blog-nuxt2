1d0430298d0a20b8319d909533065331deac484e
Nazim Boudeffa <boudeffa@gmail.com>
2022-11-02 21:13:08 +0100

    Create portfolio.vue

---
+++ pages/portfolio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="portfolio-page">
+	  <h1 class="portfolio">Portfolio Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.portfolio-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.portfolio {
+  margin: 50px 30px;
+  width: 500px;
+  height: 500px;
+  background-color: gray;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
